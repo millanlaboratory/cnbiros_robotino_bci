8799087efcb1fb9271cf1a2d97c8c84209e57084
Luca Tonin <luca.tonin@epfl.ch>
2018-02-27 14:31:27 +0100

    Added again the ros environment variables. Now it works

--- scripts/cnbiros_environment_setup.sh
+++ scripts/cnbiros_environment_setup.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-ROS_WORKSPACE_CURRENT=/mnt/data/cnbiros_ws/
+ROS_MASTER_MACHINE=bci
+ROS_LOCAL_MACHINE=robot
+ROS_WORKSPACE_CURRENT=$HOME/cnbiros_ws/
 
 # Source for the current workspace
 source $ROS_WORKSPACE_CURRENT/devel/setup.bash
+
+# Export again the ROS environment variables
+export ROS_MASTER_URI=http://$ROS_MASTER_MACHINE:11311
+export ROS_HOSTNAME=$ROS_LOCAL_MACHINE
+export ROS_IP=$(getent hosts $ROS_HOSTNAME | awk '{print $1}')
 
 exec "$@"
